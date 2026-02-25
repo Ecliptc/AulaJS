b049c368d799a09034534fdb8a9e613e7000b48b
caveiragames <steamcontaloeh@gmail.com>
2026-02-25 14:58:38 -0300

    25/02

---
+++ exercicio1.js
@@ -0,0 +1,10 @@
+document.write("<h2>Entrada")
+
+var numero = prompt("Digite o numero")
+
+var quadrado = Number(numero) * Number(numero) * Number(numero);
+var cubo = Number(numero) * Number(numero) * Number(numero);
+
+document.write("<p> O numero digitado é " + numero + "</p>")
+document.write("<p> O quadrado do numero é " + quadrado + "</p>")
+document.write("<p> O cubo do numero é " + numero + "</p>")
